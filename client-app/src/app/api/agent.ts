f83654dbbb159adaaaf5fbfa58dff7b3abf28aab
AvrahamShfaraw <83629699+AvrahamShfaraw@users.noreply.github.com>
2021-12-25 15:32:51 +0200

    Feature - Client Side attendance

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { setTimeout } from "timers";
 import { history } from "../..";
-import { Activity } from "../models/activity";
+import { Activity, ActivityFormValues } from "../models/activity";
 import { User, UserFormValues } from "../models/user";
 import { store } from "../stores/store";
 
@@ -75,9 +75,10 @@ const requests = {
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activtiy: Activity) => axios.post<void>('/activities', activtiy),
-    update: (activtiy: Activity) => axios.put<void>(`/activities/${activtiy.id}`, activtiy),
-    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
+    create: (activtiy: ActivityFormValues) => requests.post<void>('/activities', activtiy),
+    update: (activtiy: ActivityFormValues) => requests.put<void>(`/activities/${activtiy.id}`, activtiy),
+    delete: (id: string) => requests.delete<void>(`/activities/${id}`),
+    attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {})
 }
 
 const Account = {
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,7 +12,7 @@ import MyTextArea from "../../../app/common/form/MyTextArea";
 import { categoryOptions } from "../../../app/common/options/categoryOptions";
 import MySelectInput from "../../../app/common/form/MySelectInput";
 import MyDateInput from "../../../app/common/form/MyDateInput";
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 import { v4 as uuid } from 'uuid';
 
 
@@ -20,18 +20,10 @@ export default observer(function ActivtiyForm() {
 
     const history = useHistory();
     const { activityStore } = useStore();
-    const { createActivity, updateActivity, loadActivity, loadingInitial, loading } = activityStore;
+    const { createActivity, updateActivity, loadActivity, loadingInitial } = activityStore;
     const { id } = useParams<{ id: string }>();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        description: '',
-        category: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -43,11 +35,11 @@ export default observer(function ActivtiyForm() {
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!))
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
     }, [id, loadActivity]);
 
-    function handleFormSubmit(activity: Activity) {
-        if (activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues) {
+        if (!activity.id) {
             let newActivity = {
                 ...activity,
                 id: uuid()
@@ -87,7 +79,7 @@ export default observer(function ActivtiyForm() {
                         <MyTextInput placeholder='Venue' name='venue' />
                         <Button
                             disabled={isSubmitting || !dirty || !isValid}
-                            loading={loading}
+                            loading={isSubmitting}
                             floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
